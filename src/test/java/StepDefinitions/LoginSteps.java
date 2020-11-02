c1956fbfbdd4ea2bece701430206c72780413c85
Manish Upadhyay <ermanishupadhyay@gmail.com>
2020-11-02 12:15:54 +0530

    Selenium Cucumber Java BDD Framework - Sample Login Test

--- src/test/java/StepDefinitions/LoginSteps.java
+++ src/test/java/StepDefinitions/LoginSteps.java
@@ -1,30 +1,61 @@
 package StepDefinitions;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
 
 public class LoginSteps {
+	WebDriver driver = null;
+
+	@Given("use has opened the browser")
+	public void use_has_opened_the_browser() {
+		System.out.println("Inside Step - use has opened the browser");
+		String projectPath = System.getProperty("user.dir");
+		System.setProperty("webdriver.chrome.driver",projectPath + "/src/test/resources/drivers/chromedriver.exe");
+		driver = new ChromeDriver();
+		
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		
+	}
 	
-	@Given("user is on login page")
+	@And("user is on login page")
 	public void user_is_on_login_page() {
 		System.out.println("Inside step - user is on login page");
+		driver.navigate().to("https://classic.freecrm.com/");
 	}
 
 	@When("user enters username and password")
-	public void user_enters_username_and_password() {
+	public void user_enters_username_and_password() throws InterruptedException {
 		System.out.println("Inside step - user enters username and password");
+		driver.findElement(By.xpath("//input[@placeholder='Username']")).sendKeys("manishuupadhyay");
+		driver.findElement(By.xpath("//input[@placeholder='Password']")).sendKeys("Aadi@4417");
+		Thread.sleep(4000);
 	}
 
 	@And("clicks on login button")
 	public void clicks_on_login_button() {
 		System.out.println("Inside step - clicks on login button");
+		driver.findElement(By.xpath("//input[@value='Login']")).click();
 	}
 
 	@Then("user is navigated to home page")
 	public void user_is_navigated_to_home_page() {
 		System.out.println("Inside step - user is navigated to home page");
+		String pageTitle = driver.getTitle();
+		Assert.assertEquals("CRMPRO", pageTitle);
+		driver.close();
+		driver.quit();
 	}
 
 }
--- src/test/resources/Features/login.feature
+++ src/test/resources/Features/login.feature
@@ -1,11 +1,10 @@
 # Author : Manish Upadhyay
 # Date : 01-11-2020
-
 Feature: feature to test login functionality
 
   Scenario: Check login is successful with valid crediantials
-    Given user is on login page
+    Given use has opened the browser
+    And user is on login page
     When user enters username and password
     And clicks on login button
     Then user is navigated to home page
-
